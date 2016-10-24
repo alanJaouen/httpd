ddbbffc131cb0cfc44127ecabbc7dcb0a4809b0a
Alan Jaouen <alan.jaouen@epita.fr>
2016-10-24 23:18:52 +0200

    Add: src/net.c src/hello.c include/net.h

---
+++ src/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "net.h"
+
+int main()
+{
+  printest();
+	printf("hello world\n");
+}
---
+++ src/net.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void printest(void)
+{
+  printf("printest\n");
+}
