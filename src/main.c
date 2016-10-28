d0ce76d1d2ef64c453d913b960507fc3fe828466
Céline Lobry <celine.lobry@epita.fr>
2016-10-28 12:52:37 -0700

    update: src/main.c - call fetch_env function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include "fetch_env.h"
 #include "net.h"
 #include "signalhandler.h"
 
 int main()
 {
+  fetch_env();
   init_handler();
   printest();
   for (int i = 0; i < 5; i++)
